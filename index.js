0e5eb492fe3f05f4a045e66d072966a971559c7e
Orinayo Oyelade <orinayooyelade@gmail.com>
2020-11-17 07:09:13 +0100

    remove template var

--- index.js
+++ index.js
@@ -285,7 +285,7 @@ fetch('https://api.github.com/graphql', {
 	method: 'POST',
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: `bearer ${GITHUB_PAT}`,
+		Authorization: `bearer GITHUB_PAT`,
 	},
 	body: JSON.stringify({query}),
 })
